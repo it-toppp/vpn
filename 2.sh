440349d5bc304be6b0bfd3eba1edaa2aa53d6f4e
it-toppp <36742579+it-toppp@users.noreply.github.com>
2018-09-09 18:04:05 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -5,30 +5,34 @@ sudo setenforce 0
 
 #docker
 curl -fsSL https://get.docker.com/ | sh
-systemctl start docker
-systemctl enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 
 # Enable Firewall
-firewall-cmd --permanent --add-port={80,443}/tcp
-firewall-cmd --permanent--add-port={1194,1195}/udp
-firewall-cmd --reload
+sudo firewall-cmd --permanent --add-port={80,443,8081,8080}/tcp
+sudo firewall-cmd --permanent--add-port=1194/udp
+sudo firewall-cmd --reload
 pritunl setup-key
 
-mkdir -p /var/lib/openvpn/mongodb
-docker run \
+sudo mkdir -p /var/lib/openvpn/mongodb
+
+sudo docker run \
     --name openvpn \
     --privileged \
     --detach \
     --privileged \
-    --network=host \
     --restart=always \
     -p 1194:1194/udp \
     -p 1194:1194/tcp \
-    -p 80:80/tcp \
-    -p 443:443/tcp \
+    -p 8080:80/tcp \
+    -p 8081:443/tcp \
     -v /var/lib/openvpn/mongodb:/var/lib/mongodb\
+    -v /var/lib/openvpn:/var/lib/pritunl \
+    #-v /var/lib/openvpn/pritunl.conf:/etc/pritunl.conf \
     jippi/pritunl
+
+#docker exec -it openvpn /bin/bash | pritunl reset-password
     
